b374b762d5aa70ae9712b5efaba6174aad412bbb
Yee2 <13583755+yee2@users.noreply.github.com>
2022-02-09 12:29:23 +0000

    deleted:    lua/lsp.lua 	deleted:    lua/tabs.lua 	deleted:    lua/telescope.lua 	deleted:    lua/tree.lua

--- lua/telescope.lua
+++
@@ -1,9 +0,0 @@
-return function(packer)
-    packer {
-        'nvim-telescope/telescope.nvim',
-        requires = { {'nvim-lua/plenary.nvim'} },
-	config = function()
-		vim.api.nvim_set_keymap('n', '<leader>ff', [[<cmd>lua require('telescope.builtin').find_files()<cr>]],{noremap = true})
-	end
-    }
-end
